508ede2513d55d6d680e3e0fa2c1127dd85008a5
Ron <ronnachum13@gmail.com>
2020-08-26 18:57:31 -0400

    Added forms.py

---
+++ application/forms/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextField, TextAreaField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class RegisterForm(FlaskForm):
+    email = TextField('Email Address', [DataRequired()])
+    password = PasswordField('Password', [DataRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm Password')
+    submit = SubmitField('Register')
+    # accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
+
+class LoginForm(FlaskForm):
+    email = TextField('Email Address', [DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+
+class ClassForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    link = StringField('Link', validators=[DataRequired()])
+    times = StringField('Times', validators=[DataRequired()])
+    teacher = StringField('Teacher', validators=[])
+    notes = TextAreaField('Notes', validators=[])
+    submit = SubmitField('Add Class')
+
+
