c6646d4e7264008056078f1852eeb84450557c14
Ron <ronnachum13@gmail.com>
2020-08-27 13:15:46 -0400

    Added Procfile

--- application/forms/forms.py
+++ application/forms/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextField, TextAreaField, PasswordField, BooleanField
+from wtforms import StringField, SubmitField, TextField, TextAreaField, PasswordField, BooleanField, ColorField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class RegisterForm(FlaskForm):
@@ -16,6 +16,7 @@ class LoginForm(FlaskForm):
 class ClassForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     link = StringField('Link', validators=[DataRequired()])
+    color = ColorFIeld('Color', validators=[DataRequired()])
     times = StringField('Times', validators=[DataRequired()])
     teacher = StringField('Teacher', validators=[])
     notes = TextAreaField('Notes', validators=[])
