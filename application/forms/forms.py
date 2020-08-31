50394a45124cef43da8f23642adadf9e1c1ba0aa
Ron <ronnachum13@gmail.com>
2020-08-30 23:44:09 -0400

    Finished Add Class Form

--- application/forms/forms.py
+++ application/forms/forms.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired, Email, Length, EqualTo, Regexp
 minute_choices = [(1, '1'), (2, '2'), (3, '3'), (5, '5'), (10, '10'), 
                  (15, '15'), (20, '20'), (25, '25'), (30, '30')]
 minute_choices = [(x[0], x[1] + ' Minutes Before') if x[1] != '1' else (x[0], x[1] + ' Minute Before') for x in minute_choices]
+minute_choices = [(-1, "None")] + minute_choices
 
 class ClassForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -17,8 +18,8 @@ class ClassForm(FlaskForm):
         choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'),
                  ('5', '5'), ('6', '6'), ('7', '7'), ('8A', '8A'), ('8B', '8B')])
     teacher = StringField('Teacher', validators=[])
-    text_reminder = SelectField('Minutes Before', [], choices=minute_choices, default=5)
-    email_reminder = SelectField('Minutes Before', [], choices=minute_choices, default=5)
+    text_reminder = SelectField('Minutes Before', [], choices=minute_choices, default=-1)
+    email_reminder = SelectField('Minutes Before', [], choices=minute_choices, default=-1)
     notes = TextAreaField('Notes', validators=[])
     submit = SubmitField('Add Class')
 
