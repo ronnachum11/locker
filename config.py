7adc8559dfaba7ce45d004d0c1cfd5bf07ad36a0
shreygupta04 <shreygupta04@gmail.com>
2020-09-04 17:10:36 -0400

    ION Redirect working locally

--- config.py
+++ config.py
@@ -2,9 +2,34 @@ import os
 from requests_oauthlib import OAuth2Session
 from dotenv import load_dotenv
 
-def load_config(mode:str):
-    if mode == 'PRODUCTION':
-        print("PRODUCTION MODE ACTIVATED")
+def load_config():
+    # if mode == 'PRODUCTION':
+    #     print("PRODUCTION MODE ACTIVATED")
+    #     oauth_register = OAuth2Session(os.environ['REGISTER_CLIENT_ID'],
+    #                   redirect_uri='https://thelocker.io/register/ion',
+    #                   scope=["read","write"])
+
+    #     oauth_login = OAuth2Session(os.environ['LOGIN_CLIENT_ID'],
+    #                         redirect_uri='https://thelocker.io/login/ion',
+    #                         scope=["read","write"])
+    #     return oauth_register, oauth_login
+    # elif mode == 'DEBUG':
+    if os.path.exists("debug.env"):
+        print("DEBUG MODE ACTIVATED")
+
+        load_dotenv("debug.env")
+        oauth_register = OAuth2Session(os.environ['REGISTER_CLIENT_ID'],
+                      redirect_uri='http://127.0.0.1:5000/register/ion',
+                      scope=["read","write"])
+
+        oauth_login = OAuth2Session(os.environ['LOGIN_CLIENT_ID'],
+                            redirect_uri='http://127.0.0.1:5000/login/ion',
+                            scope=["read","write"])
+
+        os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+        return oauth_register, oauth_login
+    else:
         oauth_register = OAuth2Session(os.environ['REGISTER_CLIENT_ID'],
                       redirect_uri='https://thelocker.io/register/ion',
                       scope=["read","write"])
@@ -12,18 +37,4 @@ def load_config(mode:str):
         oauth_login = OAuth2Session(os.environ['LOGIN_CLIENT_ID'],
                             redirect_uri='https://thelocker.io/login/ion',
                             scope=["read","write"])
-        return oauth_register, oauth_login
-    elif mode == 'DEBUG':
-        print("DEBUG MODE ACTIVATED")
-        oauth_register = OAuth2Session(os.environ['REGISTER_CLIENT_ID'],
-                      redirect_uri='http://127.0.0.1:5000/register/ion',
-                      scope=["read","write"])
-
-        oauth_login = OAuth2Session(os.environ['REGISTER_CLIENT_ID'],
-                            redirect_uri='http://127.0.0.1:5000/login/ion',
-                            scope=["read","write"])
-        load_dotenv(".env")
-
-        os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
         return oauth_register, oauth_login
