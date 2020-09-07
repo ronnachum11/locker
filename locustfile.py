4398c7d77f7c6ff07b923c9e8cd712b9779fb7d7
Ron Nachum <ronnachum13@gmail.com>
2020-09-07 00:43:24 -0400

    Updated Locust File

--- locustfile.py
+++ locustfile.py
@@ -2,10 +2,11 @@ from locust import HttpUser, task, between
 
 class WebsiteTestUser(HttpUser):
     wait_time = between(0.5, 3.0)
-    
+
     def on_start(self):
-        self.client.post("/login", {"username":"admin", "password":"admin"})
+        pass    
 
     @task(1)
     def hello_world(self):
-        self.client.get("/home")
+        self.client.post("/login", {"username":"ronnachum13@gmail.com", "password":"password"})
+        self.client.get("/logout")
